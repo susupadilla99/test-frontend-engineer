9634115d87e03062aaae9ca1b093dac7cd86ff30
Daniel Le <lehacongduy@gmail.com>
2025-02-21 17:56:51 +1100

    Restyled the product details page. Added linking to the product details page

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,34 +1,167 @@
-'use client'
+"use client";
 
 import { ProductListing } from "@/components/typeDefinition";
-import Image from "next/image"
-import { usePathname } from "next/navigation"
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function ProductDetails() {
-    const path=usePathname();
-    const [productData, setData] = useState<ProductListing|undefined>(undefined)
+  const path = usePathname();
+  const [data, setData] = useState<ProductListing | undefined>(undefined);
 
-    useEffect( () => {
-        fetch('https://fakestoreapi.com/products/'+path.split("/")[2])
-            .then(res=>res.json())
-            .then(json => {
-                setData(json)
-            })
-    })
-    
-    if(productData === null || productData === undefined) return <div>Loading...</div>
-    
-    return (
-        <div className="border-2 border-red-300">
-        <Image src={productData.image} alt={productData.title} width={100} height={100}></Image>
-        <h3 className="font-bold text-lg">{productData.title}</h3>
-        <p>{productData.category}</p>
-        <h4 className="font-bold mt-4">Description:</h4>
-        <p className="text-gray-700 text-sm italic">{productData.description}</p>
-        <p className="mt-4">Price: ${productData.price}</p>
-        <p className="mt-4">Rating: {productData.rating.rate}/5 ({productData.rating.count} reviews)</p>
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/" + path.split("/")[2])
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+      });
+  });
+
+  if (data === null || data === undefined) return <div>Loading...</div>;
+
+  return (
+    <section className="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
+      <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
+        <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
+          {/** Image */}
+          <div className="shrink-0 max-w-md lg:max-w-lg mx-auto">
+            <Image
+              src={data.image}
+              alt={data.title}
+              className="w-full max-h-[400px]"
+              style={{ objectFit: "contain" }}
+              width={500}
+              height={400}
+            />
+          </div>
+
+          <div className="mt-6 sm:mt-8 lg:mt-0">
+            {/** Title */}
+            <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
+              {data.title}
+            </h1>
+
+            {/** Price */}
+            <div className="mt-4 sm:items-center sm:gap-4 sm:flex">
+              <p className="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white">
+                {data.price.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })}
+              </p>
+            </div>
+
+            {/** Ratings */}
+            <div className="mt-4 sm:items-center sm:gap-4 sm:flex">
+              <div className="flex items-center gap-2 mt-2 sm:mt-0">
+                <div className="flex items-center gap-1">
+                  <svg
+                    className="w-4 h-4 text-yellow-300"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"></path>
+                  </svg>
+                  <svg
+                    className="w-4 h-4 text-yellow-300"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"></path>
+                  </svg>
+                  <svg
+                    className="w-4 h-4 text-yellow-300"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"></path>
+                  </svg>
+                  <svg
+                    className="w-4 h-4 text-yellow-300"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"></path>
+                  </svg>
+                  <svg
+                    className="w-4 h-4 text-yellow-300"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"></path>
+                  </svg>
+                </div>
+                <p className="text-sm font-medium leading-none text-gray-500 dark:text-gray-400">
+                  ({data.rating.rate})
+                </p>
+                <a
+                  href="#"
+                  className="text-sm font-medium leading-none text-gray-900 underline hover:no-underline dark:text-white"
+                >
+                  {data.rating.count} Reviews
+                </a>
+              </div>
+            </div>
+
+            {/** Add to cart */}
+            <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
+              <Link
+                href="#"
+                title=""
+                className="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
+                role="button"
+              >
+                <svg
+                  className="w-5 h-5 -ms-2 me-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
+                  ></path>
+                </svg>
+                Add to cart
+              </Link>
+            </div>
+
+            <hr className="my-6 md:my-8 border-gray-200 dark:border-gray-800"></hr>
+
+            {/** Description */}
+            <p className="mb-6 text-gray-500 dark:text-gray-400">
+              {data.description}
+            </p>
+          </div>
         </div>
-    )
-    
+      </div>
+    </section>
+  );
 }
